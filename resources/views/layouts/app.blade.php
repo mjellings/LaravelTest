dccf79d947efa8b503801fbdbc8bd5aaf9872409
Matt Jellings <matt.jellings@gmail.com>
2020-02-13 16:26:05 +0000

    Added roulette link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item"><a href="/test/">Roulette</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
